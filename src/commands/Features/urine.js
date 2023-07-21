a1e5efd377afcaf5a537593ac694ef788a7b2964
notcacti <secretarjunayt@gmail.com>
2023-07-21 11:33:34 +0530

    Add functionality to /urine

--- src/commands/Features/urine.js
+++ src/commands/Features/urine.js
@@ -2,10 +2,26 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('urine')
-    .setDescription("Spits out facts about urine."),
-
+  .setName('urine')
+  .setDescription("Spits out facts about urine."),
+  
   run: async ({ interaction, client }) => {
-    await interaction.reply(`<:urine:1125441442882535555>`)
+    const getFact = async () => {
+      const getRandom = (min, max) => {
+        const rand = Math.floor(Math.random() * (max - min + 1)) + min;
+        
+        return rand;
+      }
+        const facts = await import('../../localStorage/facts.json', { assert: { type: 'json' } });
+
+        const min = 0;
+        const max = facts.default.length - 1;
+        
+        const index = await getRandom(min, max);
+
+        return facts.default[index].fact;
+    }
+
+    await interaction.reply(`**Urine Fact:** *${await getFact()}*`);
   }
 }
